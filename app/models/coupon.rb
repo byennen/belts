53958859e9ab33a54f088bc854390ea735fe6975
c4belts Dev <jasant@c4belts.com>
2013-12-10 16:24:59 +0530

    money attribute for discount + discount type

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,24 +1,34 @@
 class Coupon < ActiveRecord::Base
 
-  COUPON_TYPES = %w(fixed percentage)
+  COUPON_TYPES = %w(percentage fixed)
 
-  validates :code, presence: true, uniqueness: true
-  validates :type, inclusion: { in: COUPON_TYPES }
+  validates :code, presence: true, uniqueness: true, length: { is: 8 }
+  validates :discount_type, inclusion: { in: COUPON_TYPES }
   validates :frequency, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :discount_percentage, 
     numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 },
-    if: Proc.new { |coupon| coupon.type == 'percentage' }
+    if: Proc.new { |coupon| coupon.discount_type == 'percentage' }
   validates :discount_cents, 
     numericality: { only_integer: true, greater_than_or_equal_to: 1 },
-    if: Proc.new { |coupon| coupon.type == 'fixed' }
+    if: Proc.new { |coupon| coupon.discount_type == 'fixed' }
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  composed_of :discount,
+              :class_name => "Money",
+              :mapping => [%w(discount_cents cents), %w(discount_currency currency_as_string)],
+              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
 
   def self.generate_code
     begin
       code = SecureRandom.urlsafe_base64[0,8]
     end while Coupon.where(code: code).exists?
     code
+  end
+
+  before_validation do |coupon|
+    coupon.code = Coupon.generate_code if coupon.code.blank?
   end
 
 end
