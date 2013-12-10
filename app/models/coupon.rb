df330020a2e335ef70e2741bd06c5a335003cf88
c4belts Dev <jasant@c4belts.com>
2013-12-10 13:05:38 +0530

    validation condition on type of coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,8 +5,12 @@ class Coupon < ActiveRecord::Base
   validates :code, presence: true, uniqueness: true
   validates :type, inclusion: { in: COUPON_TYPES }
   validates :frequency, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :discount_percentage, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
-  validates :discount_cents, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :discount_percentage, 
+    numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+    if: Proc.new { |coupon| coupon.type == 'percentage' }
+  validates :discount_cents, 
+    numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+    if: Proc.new { |coupon| coupon.type == 'fixed' }
   validates :start_date, presence: true
   validates :end_date, presence: true
 
